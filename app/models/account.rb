5942f8e5b2f3fb816e0e0a86e31bf6b6ce231a82
Walther H Diechmann <walther@diechmann.net>
2024-04-23 18:49:27 +0200

    add filter to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,18 @@
 class Account < ApplicationRecord
+  has_many :filters, dependent: :destroy
+
+  scope :by_name, ->(name) { here("name LIKE ?", "%#{name}%") if name.present? }
+
+  def self.filtered(filter)
+    flt = JSON.parse filter.filter
+
+    all
+      .by_name(flt["name"])
+  rescue
+    filter.destroy if filter
+    all
+  end
+
   def self.form(resource)
     Accounts::Form.new resource
   end
