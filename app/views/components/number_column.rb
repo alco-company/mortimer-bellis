1ea18d25ece27c994cf704599fc1a693cd662a7c
Walther H Diechmann <walther@diechmann.net>
2024-04-23 16:39:07 +0200

    add columns for lists

---
+++ app/views/components/number_column.rb
@@ -0,0 +1,16 @@
+class NumberColumn < Phlex::HTML
+  include Phlex::Rails::Helpers::Routes
+
+  attr_accessor :field
+
+  def initialize(field:, css: nil)
+    @field = field
+    @class = css || "text-right"
+  end
+
+  def template
+    field.blank? ? 
+      div { "&nbsp;".html_safe } :
+      div( class: @class ) {field}
+  end
+end
