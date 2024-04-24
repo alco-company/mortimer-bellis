c00f7f21831be1616addf93c974887183383e8d2
Walther H Diechmann <walther@diechmann.net>
2024-04-24 19:33:43 +0200

    formatting - rubocop is rather picky

--- app/models/concerns/accountable.rb
+++ app/models/concerns/accountable.rb
@@ -5,7 +5,7 @@ module Accountable
     # Account is actually not optional, but we not do want
     # to generate a SELECT query to verify the account is
     # there every time. We get this protection for free
-    # because of the `Current.account_or_raise!`     
+    # because of the `Current.account_or_raise!`
     # and also through FK constraints.
     belongs_to :account, optional: true
     default_scope { where(account: Current.account_or_raise!) }
