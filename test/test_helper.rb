3c3b4c6e4baa62d8c16a124bc7bea3620d01cebd
Walther H Diechmann <walther@diechmann.net>
2024-04-24 11:46:41 +0200

    issues with tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,17 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+private_key_file = "#{Rails.root}/config/box.mortimer_3-key.pem"
+cert_chain_file = "#{Rails.root}/config/box.mortimer_3.pem"
+
+Capybara.server = :puma, { Host: "ssl://#{Capybara.server_host}?key=#{private_key_file}&cert=#{cert_chain_file}" }
+Capybara.register_driver :insecure_selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :firefox,
+    desired_capabilities: { accept_insecure_certs: true }
+  )
+end
+
+Capybara.javascript_driver = :insecure_selenium # https://github.com/teamcapybara/capybara#drivers
