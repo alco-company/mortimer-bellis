7ef6a506bbc60245b823884d97277a135427dd39
Walther H Diechmann <walther@diechmann.net>
2024-05-14 09:03:40 +0200

    make teams and locations optional (add defaults when signing up)

--- app/services/user_registration_service.rb
+++ app/services/user_registration_service.rb
@@ -2,6 +2,10 @@ class UserRegistrationService
   def self.call(user)
     if user.persisted?
       UserMailer.with(user: user).welcome.deliver_now
+      team = Team.create account: user.account, name: I18n.t("teams.default_team")
+      location = Location.create account: user.account, name: I18n.t("locations.default_location")
+      Employee.create account: user.account, name: I18n.t("employee.me"), pincode: "1234", payroll_employee_ident: "1234", team: team
+      PunchClock.create account: user.account, name: I18n.t("punch_clocks.default_punch_clock"), location: location
     end
   end
 end
