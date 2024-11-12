ad88fe621cbbbefb9308d5b9a57cbbec1b5b394f
Walther H Diechmann <walther@diechmann.net>
2024-11-12 15:22:03 +0100

    trying to fix the table.field rename issue

---
+++ db/migrate/20241112050330_change_overtime_to_integer_on_time_material.rb
@@ -0,0 +1,11 @@
+class ChangeOvertimeToIntegerOnTimeMaterial < ActiveRecord::Migration[8.0]
+  def self.up
+    # once you've verified that everything is correct, drop the old column
+    remove_column :time_materials, :old_overtime
+  end
+
+  def self.down
+    # once you've verified that everything is correct, drop the old column
+    remove_column :time_materials, :old_overtime
+  end
+end
