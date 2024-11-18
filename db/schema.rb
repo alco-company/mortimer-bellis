81fcb8bc9439dcde5e3f15d92663d2e413af6edd
Walther H Diechmann <walther@diechmann.net>
2024-11-18 23:42:03 +0100

    upgrading SolidQueue to 0.6.0 - II

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_18_222253) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_18_222255) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -450,8 +450,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_18_222253) do
     t.string "hostname"
     t.text "metadata"
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
+    t.index ["name", "supervisor_id"], name: "index_solid_queue_processes_on_name_and_supervisor_id", unique: true
     t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
   end
 
@@ -481,7 +482,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_18_222253) do
     t.text "arguments"
     t.string "queue_name"
     t.integer "priority", default: 0
-    t.boolean "static", default: true
+    t.boolean "static", default: true, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
